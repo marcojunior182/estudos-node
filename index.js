5d3c98cc7fd8661e8643b4c4a016e63bc5c27909
marcojunior182 <tombombadil182@gmail.com>
2017-09-15 13:58:47 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 import express from "express";
 import consign from "consign";
-const PORT = 3001;
+const PORT = 3000;
 
 const app = express();
-app.set("port", PORT)
 
 consign()
 .include("models")
 .then ("routes")
 .then("libs/boot.js")
 .into(app);
-
-app.listen(PORT, () => console.log(`NTask API - porta ${PORT}`));
