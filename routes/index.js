399c7af7555e5bb7db815ebf66c7aea8ad3c8caf
= <tombombadil182@gmail.com>
2017-09-15 09:32:04 -0300

    Ajustando index.js da routes e index.js principal.

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
+// app.get tem dois parametros, uma string e uma funcao callback.
 module.exports = app => {
-  app.get("/"), (req, res) =>{
-    res.json({status: "NTask API"});
-  };
+app.get("/", (req, res) =>{
+  res.json({status: "NTask API"});
+});
 };
